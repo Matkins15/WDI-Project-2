385515aa0d0f7568bb3c303b7afbcb4ed0f83cee
Matkins15 <matthew1atkins@gmail.com>
2017-03-29 11:21:35 -0400

    added wireframe images

--- db/schema.js
+++ db/schema.js
@@ -1,7 +1,30 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Meals = require('../models/meals.js');
+
 
 mongoose.Promise = global.Promise;
+
+// Meals.remove({}, function(err) {
+//     console.log(err);
+// });
+
+// Connect to database
+// if (process.env.MONGODB_URI) {
+//   mongoose.connect(process.env.MONGODB_URI);
+// }
+// else {
+//   mongoose.connect('mongodb://localhost/WDI-project-2');
+// }
+// mongoose.connection.on('error', function(err) {
+//   console.error('MongoDB connection error: ' + err);
+//   process.exit(-1);
+//   }
+// );
+// mongoose.connection.once('open', function() {
+//   console.log("Mongoose has connected to MongoDB!");
+// });
+
 
 var MealsSchema = new Schema({
     name: String,
