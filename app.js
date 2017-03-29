f233a999042643487d5ea562d36352ba56cec1e3
Matkins15 <matthew1atkins@gmail.com>
2017-03-29 08:45:42 -0400

    Updated DB connection code to use MongoLab when in Production

--- app.js
+++ app.js
@@ -18,17 +18,17 @@ var app = express();
 
 mongoose.connect('mongodb://localhost/WDI-project-2');
 // // Connect to database
-// if (process.env.MONGODB_URI) {
-//   mongoose.connect(process.env.MONGODB_URI);
-// }
-// else {
-//   mongoose.connect('mongodb://localhost/WDI-project-2');
-// }
-// mongoose.connection.on('error', function(err) {
-//   console.error('MongoDB connection error: ' + err);
-//   process.exit(-1);
-//   }
-// );
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/WDI-project-2');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
 mongoose.connection.once('open', function() {
   console.log("Mongoose has connected to MongoDB!");
 });
